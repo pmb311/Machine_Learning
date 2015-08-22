58270eed232e7c11c6989f89973f88feb064eafb
Paul Back <pmb311@gmail.com>
2015-08-22 17:10:56 -0400

    Re-factored multivariate linear regression methods.  Added mySQL support.  Added normal equation.

--- ml_basic_linear_reg/python_code/multi_lin_reg.py
+++ ml_basic_linear_reg/python_code/multi_lin_reg.py
@@ -1,55 +1,96 @@
 from __future__ import division
-from numpy import mean, std
+from my_sql_conn import mySQLconn
+from numpy import dot, mean, std
 import numpy as np
 import pandas as pd
 
-def featureNormalize(col_labels, source, sql_query=None, input_file=None):
-    if source == 'csv':
-        df = pd.read_csv(input_file, names=col_labels).drop(col_labels[-1], 1)
-	mu = pd.Series(np.zeros(2))
-	sigma = pd.Series(np.zeros(2))
-	# calculate z-score for all feature columns
-	cols = list(df.columns)
-	df_norm = pd.DataFrame()
-	for col in cols:
-		col_zscore = col + '_normalized'
-		df_norm[col_zscore] = (df[col] - df[col].mean()) / df[col].std()
-	return df_norm
+class MultiLinReg():
 
-def computeCostMulti(col_labels, theta, df=None, input_file=None):
-    if input_file != None:
-    	df = pd.read_csv(input_file, names=col_labels)
-    x = df.drop(col_labels[-1], 1)
-    m = len(x.ix[:, 0])
-    x.insert(0,'Ones', pd.Series(np.ones(m)))
-    h = pd.Series(np.dot(x, theta))
-    sqrErrors = (h - df[col_labels[-1]]) ** 2
-    J = 1 / (2 * m) * sum(sqrErrors)
-    print J
+	df = pd.DataFrame()
 
-def gradientDescentMulti(input_file, col_labels, alpha, num_iters, source, sql_query=None):
-	# Assumes that final column in the input parameter is the dependent variable
-    if source == 'csv':
-        df = pd.read_csv(input_file, names=col_labels)
-    if source == 'mySQL':
-        conn = mySQLconn
-        df = pd.read_sql(sql_query, conn)
-    # Instantiate some variables
-    m = len(df.ix[:,0])
-    J_history = pd.Series(np.zeros(len(df)))
-    # Normalize the feature matrix
-    X = featureNormalize(col_labels, source, sql_query, input_file)
-    X.insert(0,'Ones', pd.Series(np.ones(m)))
-    theta = pd.Series(np.zeros(len(X.columns)))
-    # Do gradient descent
-    for i in range(num_iters):
-    	theta = theta - (alpha / m) * np.dot(X.T, pd.Series(np.dot(X, theta)) - df[col_labels[-1]])
-    print theta
+	def __init__(self, source, col_labels=None, sql_query=None, input_file=None):
+		# initialize variables
+		self.source = source
+		self.col_labels = col_labels
+		self.sql_query = sql_query
+		self.input_file = input_file	
 
-print pd.read_csv('multi_lin_reg_data.csv', header=None, names=['sqr ft', 'BRs', 'price']).head(10)
+		# set up dataframe from mySQL or csv source data
+		if source == 'csv':
+			if col_labels == None:
+				print 'col_labels are required for the source'
+				raise TypeError
+			try:
+				MultiLinReg.df = pd.read_csv(input_file, names=col_labels)
+			except IOError:
+				print 'Error: input_file is required for this source'
+		elif source == 'mySQL':
+			try:
+				conn = mySQLconn
+				MultiLinReg.df = pd.read_sql(sql_query, conn)
+			except TypeError:
+				print 'Error: sql_query is required for this source'
+		else:
+			print 'Only csv and mySQL sources are currently supported'
+			raise TypeError
 
-hs_ft_nrml = featureNormalize(['sqrft', 'BRs', 'price'], 'csv', input_file='multi_lin_reg_data.csv')
+	def featureNormalize(self):
+		df = self.df
+		# drop the dependent variable
+		df = df.drop(df.columns[[-1]], 1)
+		# calculate z-score for all feature columns
+		cols = list(df.columns)
+		df_norm = pd.DataFrame()
+		for col in cols:
+			col_zscore = col + '_normalized'
+			df_norm[col_zscore] = (df[col] - df[col].mean()) / df[col].std()
+		print 'Printing normalized features:\n'
+		print df_norm
+		return df_norm
 
-computeCostMulti(['sqrft', 'BRs', 'price'], pd.Series(np.zeros(3)), input_file='multi_lin_reg_data.csv')
+	def computeCostMulti(self, theta):
+		df = self.df
+		# instantiate variables
+		x = df.drop(df.columns[-1], 1)
+		y = df.ix[:,-1]
+		m = len(x.ix[:, 0])
+		# add ones column
+		x.insert(0,'Ones', pd.Series(np.ones(m)))
+		# calculate the cost and assign the result to J
+		h = pd.Series(dot(x, theta))
+		sqrErrors = (h - y) ** 2
+		J = 1 / (2 * m) * sum(sqrErrors)
+		print 'Cost = ' + str(J)
+		return J
 
-gradientDescentMulti('multi_lin_reg_data.csv', ['sqrft', 'BRs', 'price'], 1, 50, 'csv')
+	def gradientDescentMulti(self, normalized_df, alpha, num_iters):
+		X = normalized_df
+		y = self.df.ix[:,-1]
+		m = len(X.ix[:,0])
+		# we will keep track of the costs in J_history
+		J_history = pd.Series(np.zeros(len(X)))
+		# Add ones column and initialize theta
+		X.insert(0,'Ones', pd.Series(np.ones(m)))
+		theta = pd.Series(np.zeros(len(X.columns)))
+		# Do gradient descent
+		for i in range(num_iters):
+			theta = theta - (alpha / m) * dot(X.T, pd.Series(dot(X, theta)) - y)
+			# FIXME insert cost into J_History J_history[i] = self.df.computeCostMulti(theta)
+		print 'Final theta values:\n' + str(theta)
+		return theta
+
+	def normalEquation(self):
+		df = self.df
+		m = len(df.ix[:,0])
+		df.insert(0,'Ones', pd.Series(np.ones(m)))
+		x = df.drop(df.columns[-1], 1)
+		y = df.ix[:,-1]
+		theta = np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
+		print "Theta found from normal equation:\n" + str(theta)
+		return theta
+
+df1 = MultiLinReg('csv', col_labels = ['abc', 'def', 'ghi'], input_file='multi_lin_reg_data.csv')
+df1_nrml = df1.featureNormalize()
+df1.computeCostMulti(pd.Series(np.zeros(3)))
+df1.gradientDescentMulti(df1_nrml, 1, 50)
+df1.normalEquation()
