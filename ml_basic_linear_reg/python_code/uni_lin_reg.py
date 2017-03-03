2270a2586ad4075dfb26a13ea58879f9fbf87511
Paul Back <pmb311@gmail.com>
2017-03-02 21:59:09 -0500

    Objectify univariate linear regression.

--- ml_basic_linear_reg/python_code/uni_lin_reg.py
+++ ml_basic_linear_reg/python_code/uni_lin_reg.py
@@ -7,78 +7,71 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from get_data_from_source import GetDataFromSource
 # sc = SparkContext('local', 'pyspark')
 
-def warmUpExercise():
-    print np.identity(5)
+class UniLinReg(GetDataFromSource):
+    def plotDataExample(self, presentation=False):
+        if presentation:
+            fig = plt.figure()
+        plt.scatter(self.X['Population'], self.y)
+        plt.xlabel('Population')
+        plt.ylabel('Profit')
+        plt.show()
+        if presentation:
+            fig.suptitle('Profit By Population')
+            fig.savefig('test.jpg')
 
-def plotDataFromCsv(input_file, x, y, presentation=False):
-    df = pd.read_csv(input_file, names=[x, y])
-    if presentation:
-        fig = plt.figure()
-    plt.scatter(df[x], df[y])
-    plt.xlabel('Population')
-    plt.ylabel('Profit')
-    plt.show()
-    if presentation:
-        fig.suptitle('Profit By Population')
-        fig.savefig('test.jpg')
+    def computeCost(self, theta):
+        m = len(self.y)
+        h = pd.Series(np.dot(self.X, theta))
+        sqrErrors = (h - self.y) ** 2
+        J = 1 / (2 * m) * sum(sqrErrors)
+        return J
 
-def computeCost(x, y, theta, input_file):
-    df = pd.read_csv(input_file, names=[x, y])
-    m = len(df[y])
-    df.insert(0,'Ones', pd.Series(np.ones(m)))
-    h = pd.Series(np.dot(df.ix[:,'Ones':x], theta))
-    sqrErrors = (h - df[y]) ** 2
-    J = 1 / (2 * m) * sum(sqrErrors)
-    print J
-
-def sparkComputeCost(input_file, x, y, theta):
-    df = pd.read_csv(input_file, names=[x, y])
-    m = len(df[y])
-    df.insert(0,'Ones', pd.Series(np.ones(m)))
-    df.insert(0,'h', pd.Series(np.dot(df.ix[:,'Ones':x], theta)))
-    sqlCtx = SQLContext(sc)
-    spark_df = sqlCtx.createDataFrame(df)
-    spark_df = spark_df.withColumn('sqrErrors', psf.pow(spark_df.h - spark_df.Profit, 2)) #FIXME hardcoded to test example
-    J = spark_df.select(1 / (2 * m) * psf.sum(spark_df.sqrErrors))
-    J.show()
-    
-
-def gradientDescent(input_file, x, y, alpha, num_iters, source, sql_query=None):
-    if source == 'csv':
+    def sparkComputeCost(self, input_file, x, y, theta):
         df = pd.read_csv(input_file, names=[x, y])
-    if source == 'mySQL':
-        conn = mySQLconn
-        df = pd.read_sql(sql_query, conn)
-    m = len(df[y])
-    df.insert(0,'Ones', pd.Series(np.ones(m)))
-    J_history = pd.Series(np.zeros(len(df)))
-    X = df.ix[:,'Ones':x].T
-    theta = pd.Series(np.zeros(len(df.columns) - 1))
-    for i in range(num_iters):
-        theta = theta - (alpha / m) * np.dot(X, pd.Series(np.dot(df.ix[:,'Ones':x], theta)) - df.ix[:, y])
-        J_history[i] = computeCost(x, y, theta, input_file)
-    print theta
-    # print J_history
-    # graph it
-    plt.scatter(df[x], df[y])
-    plt.plot(df[x], theta[1] * df[x] + theta[0], '-')
-    plt.xlabel('Population')
-    plt.ylabel('Profit')
-    plt.show()
-    predict1 = 10000 * sum(pd.Series([1, 3.5]) * theta)
-    predict2 = 10000 * sum(pd.Series([1, 7]) * theta)
-    print 'For population = 35,000, we predict a profit of ' + str(predict1)
-    print 'For population = 70,000, we predict a profit of ' + str(predict2)
+        m = len(df[y])
+        df.insert(0,'Ones', pd.Series(np.ones(m)))
+        df.insert(0,'h', pd.Series(np.dot(df.ix[:,'Ones':x], theta)))
+        sqlCtx = SQLContext(sc)
+        spark_df = sqlCtx.createDataFrame(df)
+        spark_df = spark_df.withColumn('sqrErrors', psf.pow(spark_df.h - spark_df.Profit, 2)) #FIXME hardcoded to test example
+        J = spark_df.select(1 / (2 * m) * psf.sum(spark_df.sqrErrors))
+        J.show()
+        
+    def gradientDescent(self, alpha, num_iters, debug=False):
+        X = self.X
+        y = self.y
+        
+        m = len(y)
+        J_history = pd.Series(np.zeros(len(y)))
+        theta = pd.Series(np.zeros(len(X.columns)))
+        for i in range(num_iters):
+            theta = theta - (alpha / m) * np.dot(X.T, pd.Series(np.dot(X, theta)) - y)
+            J_history[i] = self.computeCost(theta)
+        if debug:
+            print "Theta\n"
+            print theta
+            print "J_history\n"
+            print J_history
+        # graph it
+        plt.scatter(X['Population'], y)
+        plt.plot(X, theta[1] * X + theta[0], '-')
+        plt.xlabel('Population')
+        plt.ylabel('Profit')
+        plt.show()
+        predict1 = 10000 * sum(pd.Series([1, 3.5]) * theta)
+        predict2 = 10000 * sum(pd.Series([1, 7]) * theta)
+        print 'For population = 35,000, we predict a profit of ' + str(predict1)
+        print 'For population = 70,000, we predict a profit of ' + str(predict2)
 
-
-warmUpExercise()
-
-plotDataFromCsv('uni_lin_reg_data.csv', 'Population', 'Profit')
-
-computeCost('Population', 'Profit', pd.Series(np.zeros(2)), 'uni_lin_reg_data.csv') 
-
-gradientDescent('uni_lin_reg_data.csv', 'Population', 'Profit', 0.01, 1500, 'mySQL', 'select Population, Profit from uni_lin_reg_data')
+if __name__ == "__main__":
+    print "Running test case..."
+    uniLinReg = UniLinReg('mySQL', sql_query='select Population, Profit from uni_lin_reg_data')
+    uniLinReg.plotDataExample(True)
+    uniLinReg.computeCost(pd.Series(np.zeros(2))) 
+    uniLinReg.gradientDescent(0.01, 1500, debug=True)
+    print "Done test case..."
 
 # sparkComputeCost('uni_lin_reg_data.csv', 'Population', 'Profit', pd.Series(np.zeros(2)))
